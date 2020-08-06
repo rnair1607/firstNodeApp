1cd595ac7cf14d24d70d7dffb529ddad6fa73eae
Rahul Nair <rnair1607@gmail.com>
2020-08-06 20:52:30 +0530

    response with urls and getting the data in txt

---
+++ app.js
@@ -0,0 +1,44 @@
+const http = require("http");
+const fs = require("fs");
+
+// const reqListener =
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  message = "";
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      `<body>
+          <form action='/message' method='POST'>
+              <input type='text' name='message'>
+              <button type='submit'>Send</button>
+          </form>
+          </body>`
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      //   console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    // res.writeHead(302,{});
+    // res.write(`<h1>${message}</h1>`);
+    return res.end();
+  }
+});
+
+server.listen(8000);
