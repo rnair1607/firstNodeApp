973ce35907f7c3f31de1c85746e99017bd6f72c1
Rahul Nair <rnair1607@gmail.com>
2020-08-08 12:53:10 +0530

    npm init

--- app.js
+++ app.js
@@ -1,45 +1,10 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
 
 // const reqListener =
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  message = "";
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      `<body>
-          <form action='/message' method='POST'>
-              <input type='text' name='message'>
-              <button type='submit'>Send</button>
-          </form>
-          </body>`
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
+console.log(routes.text);
 
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Hello</h1>");
-});
+const server = http.createServer(routes.handler);
 
 server.listen(8000);
