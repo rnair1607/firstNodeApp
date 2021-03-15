b9cdafb93f4b8002bbab73033f06b44adc254e34
Rahul Nair <rnair1607@gmail.com>
2021-03-16 00:10:10 +0530

    finished EJS

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -10,17 +10,19 @@ const { dirname } = require("path");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
+// app.engine(
+//   "hbs",
+//   expressHbs({
+//     layoutsDir: "views/layouts/",
+//     defaultLayout: "main-layout",
+//     extname: "hbs",
+//   })
+// );
 
-app.set("view engine", "hbs");
+// app.set("view engine", "hbs");
 // app.set("view engine", "pug");
+
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,7 +32,7 @@ app.use("/admin", adminRoutes.router);
 app.use(shopRoutes.router);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { docTitle: "Page not found!!" });
+  res.status(404).render("404", { docTitle: "Page not found!!", path: "/404" });
 });
 
 app.listen(8000);
